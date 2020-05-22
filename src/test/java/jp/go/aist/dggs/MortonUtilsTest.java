219e6462b3df6820858ca19aeef1b187545c50b1
taehoonkim <taehoon.kim@pusan.ac.kr>
2020-05-22 11:37:05 +0900

    modified a H_RANGE to 16000

--- src/test/java/jp/go/aist/dggs/MortonUtilsTest.java
+++ src/test/java/jp/go/aist/dggs/MortonUtilsTest.java
@@ -28,33 +28,34 @@ public class MortonUtilsTest {
     @Test
     public void convertToMorton() {
         String mortonCode = MortonUtils.convertToMorton(34.6400223819d, 135.454610432d, 1.1d, 32);
-        assertEquals("803213125333023101316260525667721", mortonCode);
+        assertEquals("803213121733023101312224125223321", mortonCode);
 
         Coordinate coords = MortonUtils.convertFromMorton(mortonCode);
         assertEquals(34.64002238, Math.round(coords.getX()*10000000)/10000000.0, 0.0000001);
         assertEquals(135.45461043, Math.round(coords.getY()*10000000)/10000000.0, 0.0000001);
-        assertEquals(1.1, Math.round(coords.getZ()*10000)/10000.0, 0.00001);
+        assertEquals(1.1, Math.round(coords.getZ()*10000)/10000.0, 0.01);
     }
 
     @Test
     public void convertToMorton2() {
         String mortonCode = MortonUtils.convertToMorton(34.6400223819d, 135.454610432d, 1.1d, 30);
 
-        assertEquals("8032131253330231013162605256677", mortonCode);
+        assertEquals("8032131217330231013122241252233", mortonCode);
         Coordinate coords = MortonUtils.convertFromMorton(mortonCode);
         assertEquals(34.6400224, Math.round(coords.getX()*10000000)/10000000.0, 0.00000001);
         assertEquals(135.4546104, Math.round(coords.getY()*10000000)/10000000.0, 0.00000001);
-        assertEquals(1.1, Math.round(coords.getZ()*10000)/10000.0, 0.00001);
+        assertEquals(1.1, Math.round(coords.getZ()*10000)/10000.0, 0.01);
     }
 
     @Test
     public void convertToMorton_resolution() {
         String mortonCode = MortonUtils.convertToMorton(34.6400223819d, 135.454610432d, 20d, 30);
-        Coordinate coords = MortonUtils.convertFromMorton(mortonCode, 14);
+        Coordinate coords = MortonUtils.convertFromMorton(mortonCode, 16);
         System.out.println(coords.getX());
         System.out.println(coords.getY());
         System.out.println(coords.getZ());
         assertEquals(34.64, Math.round(coords.getX()*100)/100.0, 0.001);
         assertEquals(135.45, Math.round(coords.getY()*100)/100.0, 0.001);
+        assertEquals(0, Math.round(coords.getZ()*10000)/10000.0, 0.01);
     }
 }
