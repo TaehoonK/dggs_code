26bec575e98d932d6ee1ad02f436233c658917bb
[PNU]Taehoon Kim <taehoon.kim@pusan.ac.kr>
2020-07-03 21:18:49 +0900

    Bug fix related with ISEA4DFaceCoordinates.toOrthogonal function

--- src/test/java/jp/go/aist/dggs/MortonUtilsTest.java
+++ src/test/java/jp/go/aist/dggs/MortonUtilsTest.java
@@ -10,8 +10,8 @@ import org.giscience.utils.geogrid.projections.ISEAProjection;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static jp.go.aist.dggs.common.DGGS.*;
+import static org.junit.Assert.*;
 
 public class MortonUtilsTest {
     private final double _precision = 1e-7;
@@ -37,7 +37,7 @@ public class MortonUtilsTest {
     }
 
     @Test
-    public void convertToMorton() throws Exception {
+    public void convertToMorton() {
         String mortonCode = MortonUtils.toPDCode(new GeoCoordinates(34.6400223819d, 135.454610432d, 1.1d), 32);
         GeoCoordinates coords = MortonUtils.toGeoCoordinate(mortonCode);
         assertTrue(Math.abs(34.6400223819d - coords.getLat()) < this._precision);
@@ -46,7 +46,7 @@ public class MortonUtilsTest {
     }
 
     @Test
-    public void convertToMorton_Random() throws Exception {
+    public void convertToMorton_Random() {
         for (int i = 0; i < this._iterations; i++) {
             GeoCoordinates c = new GeoCoordinates(Math.random() * 179.99 - 89.995, Math.random() * 360.0 - 180.0, Math.random() * DGGS.H_RANGE * 2 - DGGS.H_RANGE);
             String mortonCode = MortonUtils.toPDCode(c, DGGS.MAX_XY_RESOLUTION);
@@ -59,7 +59,34 @@ public class MortonUtilsTest {
     }
 
     @Test
-    public void convertToMorton_resolution() throws Exception {
+    public void orthogonal_Random() {
+        for (int i = 0; i < this._iterations; i++) {
+            GeoCoordinates c = new GeoCoordinates(Math.random() * 179.99 - 89.995, Math.random() * 360.0 - 180.0, Math.random() * DGGS.H_RANGE * 2 - DGGS.H_RANGE);
+            ISEA4DFaceCoordinates faceCoordinates = MortonUtils.toFaceCoordinate(c).toOrthogonal();
+
+            ISEAProjection p = new ISEAProjection();
+            FaceCoordinates f = p.sphereToIcosahedron(c);
+            int face = f.getFace();
+            double newPointX;
+            double newPointY;
+            newPointX = f.getX() - NEW_ORIG_X;
+            if ((face >= 0 && face <= 4) || (face >= 10 && face <= 14)) {
+                newPointY = f.getY() - NEW_ORIG_Y;
+            } else {
+                newPointY = f.getY() + NEW_ORIG_Y;
+            }
+            double origX = newPointX / (-NEW_ORIG_X * 2) * TOTAL_RANGE;
+            double origY = newPointY / (-NEW_ORIG_X * Math.sqrt(3)) * TOTAL_RANGE;
+            double origZ = ((H_RANGE + c.getHeight()) / (H_RANGE * 2.0d)) * TOTAL_RANGE_Z;
+
+            assertTrue(Math.abs(faceCoordinates.getX() - Double.valueOf(origX).longValue()) <= 1);
+            assertTrue(Math.abs(faceCoordinates.getY() - Double.valueOf(origY).longValue()) <= 1);
+            assertTrue(Math.abs(faceCoordinates.getZ() - Double.valueOf(origZ).longValue()) <= 1);
+        }
+    }
+
+    @Test
+    public void convertToMorton_resolution() {
         GeoCoordinates c = new GeoCoordinates(Math.random() * 179.99 - 89.995, Math.random() * 360.0 - 180.0);
         String mortonCode = MortonUtils.toPDCode(c, DGGS.MAX_XY_RESOLUTION);
         GeoCoordinates coords = MortonUtils.toGeoCoordinate(mortonCode, DGGS.MAX_XY_RESOLUTION);
@@ -80,7 +107,7 @@ public class MortonUtilsTest {
     }
 
 
-    public void convertToMorton_TriangleCheck() throws Exception {
+    public void convertToMorton_TriangleCheck() {
         double _goldenRatio = (1 + Math.sqrt(5)) / 2.;
         double F_DEG = Math.toDegrees(Math.atan(1 / (2 * Math.pow(_goldenRatio, 2))));
         double _g = F_DEG + 2 * Math.toDegrees(Math.atan(_goldenRatio)) - 90;
@@ -149,7 +176,7 @@ public class MortonUtilsTest {
         System.out.println("20:" + coords.toString());
     }
 
-    public void convertToMorton_Check_MIN() throws Exception {
+    public void convertToMorton_Check_MIN() {
         double epsilon = 0.00000000000001;
         double _V_lat = 26.56505120675770;//26.565051177;
         double _V_lon = -180d + 0.0000001;
@@ -174,7 +201,7 @@ public class MortonUtilsTest {
         System.out.println("Min_Y : " + f.getY());
     }
 
-    public void convertMortonToLatLong3D() throws Exception {
+    public void convertMortonToLatLong3D() {
         for (int resolution = 0; resolution <= 32; resolution++) {
             double latitude = 35d;
             double longitude = 135d;
